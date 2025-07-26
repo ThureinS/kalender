c7a0ca77c5be42cb93b7e860d546e310657d027f
ThureinS <94038134+ThureinS@users.noreply.github.com>
2025-07-26 12:39:28 +0630

    Database connect start

---
+++ app/(auth)/login/[[...login]]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Image from "next/image";
+import {SignIn} from "@clerk/nextjs";
+
+const LoginPage = () => {
+    return (
+        <main className="flex flex-col items-center p-5 gap-10 animate-fade-in">
+            <Image src='/assets/logo.svg' alt="Logo" width={100} height={100} />
+
+            <div className="mt-3">
+                <SignIn/>
+            </div>
+        </main>
+    );
+};
+
+export default LoginPage;
