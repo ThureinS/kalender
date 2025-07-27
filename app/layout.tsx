b8a371eae7a9d544533c3da312a0b6ea57960881
ThureinS <94038134+ThureinS@users.noreply.github.com>
2025-07-28 02:31:13 +0630

    Complete project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,39 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {ClerkProvider} from "@clerk/nextjs";
-import {Toaster} from "sonner";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Kalender",
-  description: "A calendar and appointment making website",
+    title: "Kalender",
+    description: "Kalender is a simple and efficient calendar app that helps you manage your events, meetings, and schedules with ease. Stay organized and never miss an important date again!",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <ClerkProvider>
-        <html lang="en">
-        <body
-            cz-shortcut-listen="true"
-            className={`${geistSans.variable} ${geistMono.variable} antialiased animate-fade-in`}
-        >
-        {children}
-        <Toaster/>
-        </body>
-        </html>
-    </ClerkProvider>
-  );
+    return (
+        <ClerkProvider>
+            <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased animate-fade-in`}
+            >
+            {children}
+            <Toaster />
+            </body>
+            </html>
+        </ClerkProvider>
+    );
 }
